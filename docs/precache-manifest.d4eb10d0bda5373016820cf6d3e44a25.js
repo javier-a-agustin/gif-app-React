81d186e3d5a189c694317ed13eaedc4d54fea8d2
Javier A. Fernandez <22.javier.fernandez@gmail.com>
2020-08-02 05:17:02 -0300

    first commit

---
+++ docs/precache-manifest.d4eb10d0bda5373016820cf6d3e44a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adce3938191520af521bca4d9026c9fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "526c74e0c7cb33c70641",
+    "url": "/static/css/main.4085c50e.chunk.css"
+  },
+  {
+    "revision": "601b0d55f32663f658c7",
+    "url": "/static/js/2.7f357064.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f357064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526c74e0c7cb33c70641",
+    "url": "/static/js/main.1d5c4774.chunk.js"
+  },
+  {
+    "revision": "39e88660e5d512526393",
+    "url": "/static/js/runtime-main.3c337214.js"
+  }
+]);
